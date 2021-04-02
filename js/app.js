d4a0e877d3003c210f0c70619f9bac773d057bdd
Tatsuya Maeta <tat.mae084@gmail.com>
2021-04-02 21:30:49 +0900

    fixed files

--- js/app.js
+++ js/app.js
@@ -1,25 +1,72 @@
 let nameV, rollV, secV, genV;
 
-var f_data = firebase.database(); 
+// var f_data = firebase.database();
+
+//---------------------------- データ準備 ----------------------------
 
 function Ready() {
-    nameV = document.getElementById("namebox").Value;
-    rollV = document.getElementById("rollbox").Value;
-    secV = document.getElementById("sectionbox").Value;
-    genV = document.getElementById("genderbox").Value;
+    nameV = document.getElementById("namebox").value;
+    rollV = document.getElementById("rollbox").value;
+    secV = document.getElementById("sectionbox").value;
+    genV = document.getElementById("genderbox").value;
 }
 
-//--------------------- 挿入処理 ---------------------
+//---------------------------- 挿入処理 ----------------------------
 
 document.getElementById("insert").onclick = function () {
     Ready();
+    console.dir(firebase.database());
     firebase
         .database()
         .ref("student/" + rollV)
         .set({
+            NameOfStudent: nameV, //(Key: Value)
+            RollNo: rollV, //(Key: Value)
+            Section: secV, //(Key: Value)
+            Gender: genV, //(Key: Value)
+        });
+};
+
+//---------------------------- 項目選択 ----------------------------
+document.getElementById("select").onclick = function () {
+    Ready();
+
+    firebase
+        .database()
+        .ref("student/" + rollV)
+        .on("value", function (snapshot) {
+            document.getElementById(
+                "namebox"
+            ).value = snapshot.val().NameOfStudent;
+            document.getElementById(
+                "sectionbox"
+            ).value = snapshot.val().Section;
+            document.getElementById("genderbox").value = snapshot.val().Gender;
+        });
+};
+
+//---------------------------- 更新処理 ----------------------------
+
+document.getElementById("update").onclick = function () {
+    Ready();
+    console.dir(firebase.database());
+    firebase
+        .database()
+        .ref("student/" + rollV)
+        .update({
             NameOfStudent: nameV,
-            RollNo: rollV,
             Section: secV,
             Gender: genV,
         });
+};
+
+//---------------------------- 削除処理 ----------------------------
+
+document.getElementById("delete").onclick = function () {
+    Ready();
+    console.dir(firebase.database());
+    firebase
+        .database()
+        .ref("student/" + rollV)
+        .remove();
 };
